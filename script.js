71745c775895f5ac0e6f391f3d95bca8757126ae
Adil <42942677+adil93s@users.noreply.github.com>
2024-01-31 22:56:56 +0100

    Fixed issue with z-index

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const rightBtn = document.getElementById("rightBtn");
 
 document.getElementById("nav-open").addEventListener("click", function () {
     document.getElementById("nav").classList.toggle("opacity-0");
+    document.getElementById("nav").classList.toggle("-z-10");
     document.getElementById("nav").classList.add("opacity-100");
+    document.getElementById("nav").classList.add("z-50");
 });
 
 var navClose = document.getElementsByClassName("nav-close");
@@ -14,7 +16,9 @@ var navClose = document.getElementsByClassName("nav-close");
 Array.from(navClose).forEach(function (button) {
     button.addEventListener("click", function () {
         document.getElementById("nav").classList.toggle("opacity-100");
+        document.getElementById("nav").classList.toggle("z-50");
         document.getElementById("nav").classList.add("opacity-0");
+        document.getElementById("nav").classList.add("-z-10");
     });
 });
 
